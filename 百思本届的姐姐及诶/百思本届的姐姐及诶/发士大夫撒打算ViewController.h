a2bd4e5ff64cb389555d1be6bcd861bde8c62040
why <why@163.com>
2016-02-17 09:14:29 +0800

    colleciton的创建

---
+++ 百思本届的姐姐及诶/百思本届的姐姐及诶/发士大夫撒打算ViewController.h
@@ -0,0 +1,13 @@
+//
+//  发士大夫撒打算ViewController.h
+//  百思本届的姐姐及诶
+//
+//  Created by 张呈成 on 16/2/17.
+//  Copyright © 2016年 张呈成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _______ViewController : UIViewController
+
+@end
---
+++ 百思本届的姐姐及诶/百思本届的姐姐及诶/发士大夫撒打算ViewController.m
@@ -0,0 +1,51 @@
+//
+//  发士大夫撒打算ViewController.m
+//  百思本届的姐姐及诶
+//
+//  Created by 张呈成 on 16/2/17.
+//  Copyright © 2016年 张呈成. All rights reserved.
+//
+
+#import "发士大夫撒打算ViewController.h"
+
+@interface _______ViewController ()
+
+@end
+
+@implementation _______ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"asdfasf");
+    
+    
+    UICollectionViewFlowLayout * layout =  [[UICollectionViewFlowLayout alloc] init];
+    
+    UICollectionView * collection  = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 99, 9, 0) collectionViewLayout:layout];
+    
+    NSLog(@"%@",collection);
+    
+    
+    
+
+
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
